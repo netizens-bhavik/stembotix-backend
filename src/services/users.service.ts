07759422ae01880360ff6724a4f542045d7a4d7f
Jeet Patel <jeet.p@netizens.email>
2022-12-09 08:40:11 +0000

    Profile crud

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,20 +1,26 @@
-import { hash } from "bcrypt";
-import DB from "@databases";
-import { RegisterUserDto, LoginUserDto } from "@dtos/users.dto";
-import { HttpException } from "@exceptions/HttpException";
-import { User } from "@interfaces/users.interface";
-import { isEmpty } from "@utils/util";
+import { hash } from 'bcrypt';
+import DB from '@databases';
+import { RegisterUserDto, LoginUserDto } from '@dtos/users.dto';
+import { HttpException } from '@exceptions/HttpException';
+import { User } from '@interfaces/users.interface';
+import { isEmpty } from '@utils/util';
 
 class UserService {
   public users = DB.User;
 
-  public async findAllUser(): Promise<User[]> {
+  public isAdmin(userData): boolean {
+    return userData.role === 'admin';
+  }
+
+  public async findAllUser(loggedUser): Promise<User[]> {
+    if (!this.isAdmin(loggedUser)) throw new HttpException(401, 'Unauthorized');
     const allUser: User[] = await this.users.findAll();
     return allUser;
   }
 
-  public async findUserById(userId: number): Promise<User> {
-    if (isEmpty(userId)) throw new HttpException(400, "UserId is empty");
+  public async findUserById(loggedUser, userId: string): Promise<User> {
+    if (!this.isAdmin(loggedUser)) throw new HttpException(401, 'Unauthorized');
+    if (isEmpty(userId)) throw new HttpException(400, 'UserId is empty');
 
     const findUser: User = await this.users.findByPk(userId);
     if (!findUser) throw new HttpException(409, "User doesn't exist");
@@ -22,31 +28,14 @@ class UserService {
     return findUser;
   }
 
-  public async createUser(userData: RegisterUserDto): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "userData is empty");
-
-    const findUser: User = await this.users.findOne({
-      where: { email: userData.email },
-    });
-    if (findUser)
-      throw new HttpException(
-        409,
-        `This email ${userData.email} already exists`
-      );
-
-    const hashedPassword = await hash(userData.password, 10);
-    const createUserData: User = await this.users.create({
-      ...userData,
-      password: hashedPassword,
-    });
-    return createUserData;
-  }
-
   public async updateUser(
-    userId: number,
+    loggedUser,
+    userId: string,
     userData: RegisterUserDto
   ): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "userData is empty");
+    if (!(loggedUser.id === userId || loggedUser.role === 'admin'))
+      throw new HttpException(403, 'Access Forbidden');
+    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
 
     const findUser: User = await this.users.findByPk(userId);
     if (!findUser) throw new HttpException(409, "User doesn't exist");
@@ -61,7 +50,9 @@ class UserService {
     return updateUser;
   }
 
-  public async deleteUser(userId: number): Promise<User> {
+  public async deleteUser(loggedUser, userId: string): Promise<User> {
+    if (!(loggedUser.id === userId || loggedUser.role === 'admin'))
+      throw new HttpException(403, 'Access Forbidden');
     if (isEmpty(userId)) throw new HttpException(400, "User doesn't existId");
 
     const findUser: User = await this.users.findByPk(userId);
