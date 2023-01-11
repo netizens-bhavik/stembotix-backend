9a65ba504c6c10d41395b2ecd95fc2e4ca12b763
NamanShah12345 <naman@technoproject.in>
2023-01-11 22:55:13 +0530

    model

--- src/controllers/likedislike.controller.ts
+++ src/controllers/likedislike.controller.ts
@@ -11,9 +11,13 @@ class LikeDislikeController {
         next: NextFunction
     )=>{
         try{
-            const {likeDislike,comment_id}=req.body
+            const {likedislike,comment_id}=req.body
             const user = req.user
-            
+             const response:LikeDislike=await this.likeDislikeService.addLike({
+                likedislike,
+                comment_id
+             })
+             res.status(200).send({respoonse:response,message:"like successfull"})
         }catch(error){
             next(error)
         }
