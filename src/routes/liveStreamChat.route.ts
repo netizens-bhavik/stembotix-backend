e17c9ef41e67bc2f6ac48e54d37eb6d94f946234
Shashang  Nargolwala <shashang.n@netizens.email>
2023-02-23 04:10:53 +0000

    Upload image with socket.io

---
+++ src/routes/liveStreamChat.route.ts
@@ -0,0 +1,49 @@
+import { Router } from 'express';
+import validationMiddleware from '@middlewares/validation.middleware';
+import { Routes } from '@interfaces/routes.interface';
+import passport from 'passport';
+import passportConfig from '@/config/passportConfig';
+import LiveStreamChatController from '@controllers/liveStreamChat.controller';
+import {
+  LiveStreamChatDTO,
+  LiveStreamChatGetDTO,
+} from '@/dtos/livestreamchat.dto';
+// import 'reflect-metadata';
+
+class LiveStreamChatRoute implements Routes {
+  public path = '/livestreamchat';
+  public passport = passportConfig(passport);
+  public router = Router();
+  public liveStreamchatController = new LiveStreamChatController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+  private initializeRoutes() {
+    this.router.post(
+      `${this.path}/:livestreamId`,
+      [
+        passport.authenticate('jwt', { session: false }),
+        validationMiddleware(LiveStreamChatDTO, 'body'),
+      ],
+      this.liveStreamchatController.sendLiveStreamChat
+    );
+
+    this.router.delete(
+      `${this.path}/:message_id`,
+      [passport.authenticate('jwt', { session: false })],
+      this.liveStreamchatController.deleteLiveStreamChat
+    );
+
+    this.router.get(
+      `${this.path}/:livestreamId`,
+      [
+        passport.authenticate('jwt', { session: false }),
+        validationMiddleware(LiveStreamChatGetDTO, 'params'),
+      ],
+      this.liveStreamchatController.getLiveStreamChat
+    );
+  }
+}
+
+export default LiveStreamChatRoute;
