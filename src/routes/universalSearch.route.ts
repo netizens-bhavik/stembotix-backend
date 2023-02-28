bec3e0549c32e2eb0c7a4ad85cebf66912c146c8
Shubham  Damania <shubham.d@netizens.email>
2023-02-28 10:05:24 +0000

    Universal search

---
+++ src/routes/universalSearch.route.ts
@@ -0,0 +1,25 @@
+import UniversalSearchController from '@/controllers/universalSearch.controller';
+import { Router } from 'express';
+import { Routes } from '@interfaces/routes.interface';
+import passport from 'passport';
+import passportConfig from '@/config/passportConfig';
+
+class UniversalSearchRoute implements Routes {
+  public path = '/universal';
+  public router = Router();
+  public passport = passportConfig(passport);
+  public universalSearchController = new UniversalSearchController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+  private initializeRoutes() {
+    //Universal search
+    this.router.get(
+      `${this.path}/search`,
+      passport.authenticate('jwt', { session: false }),
+      this.universalSearchController.universalSearch
+    );
+  }
+}
+export default UniversalSearchRoute;
