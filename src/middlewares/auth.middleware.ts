15f57d06f544be23c0837765ec6fe6772f0233c8
Shashang0711 <shashangtandel143@gmail.com>
2023-04-05 18:08:54 +0530

    bug fixes

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Response } from "express";
-import { verify } from "jsonwebtoken";
-import { SECRET_KEY } from "@config";
-import DB from "@databases";
-import { HttpException } from "@exceptions/HttpException";
-import { DataStoredInToken, RequestWithUser } from "@interfaces/auth.interface";
+import { NextFunction, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { SECRET_KEY } from '@config';
+import DB from '@databases';
+import { HttpException } from '@exceptions/HttpException';
+import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
 
 const authMiddleware = async (
   req: RequestWithUser,
@@ -11,7 +11,7 @@ const authMiddleware = async (
   next: NextFunction
 ) => {
   try {
-    const token = req.headers["x-access-token"];
+    const token = req.headers['x-access-token'];
 
     if (token) {
       const secretKey: string = SECRET_KEY;
@@ -23,16 +23,16 @@ const authMiddleware = async (
       const findUser = await DB.Users.findByPk(userId);
 
       if (findUser) {
-        req.user?.id = findUser;
+        req.user.id = findUser;
         next();
       } else {
-        next(new HttpException(401, "Wrong authentication token"));
+        next(new HttpException(401, 'Wrong authentication token'));
       }
     } else {
-      next(new HttpException(404, "Authentication token missing"));
+      next(new HttpException(404, 'Authentication token missing'));
     }
   } catch (error) {
-    next(new HttpException(401, "Wrong authentication token"));
+    next(new HttpException(401, 'Wrong authentication token'));
   }
 };
 
